a2800b87fb17baeae4b1cecc8b7883da77f47b76
kdb <dabin6743@gmail.com>
2023-01-31 18:36:05 +0900

    큰 데이터 state에 넣기

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
+
 
 function App() {
+
+
+  let [shose,setShose] = useState();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='main-bg'></div>
+
+      <div className='container'>
+        <Row>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes4.jpg"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes7.jpg"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </div>
+     
+
+
+
+
     </div>
   );
 }
