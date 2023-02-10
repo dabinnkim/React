06bda39ca92dd8c74fef8c2b769b8bda3928b67f
kdb <dabin6743@gmail.com>
2023-02-10 23:24:15 +0900

    useEffect

--- shoppingmall/src/compo/Cart.js
+++ shoppingmall/src/compo/Cart.js
@@ -1,29 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import {useParams, useSearchParams} from 'react-router-dom'
+import '../App.css';
 
 const Cart = (props) => {
 
-    useEffect(()=>{
-        console.log('ㅎㅇ')
-    })
-
-    let[count,setCount] = useState(0)
-
-    let {usernum} = useParams(); // 사용자가 입력한 url 파라미터를 가져와줌
+    // 사용자가 입력한 url 파라미터를 가져와줌
+    let {usernum} = useParams(); 
 
     //usernum과 같은 id를 가진 상품을 출력해야됨
     let 상품 = props.shoes.find(num => num.id==usernum) 
+
+
+    let[count,setCount] = useState(0)
+
+    let [alert,setAlert] = useState(true)
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{setAlert(false)},2000) //2초 후에 코드 실행
+        console.log(2)
+        return()=>{
+            console.log(1)
+            clearTimeout(a)
+        }
+    })
     
 
     return (
         <div className="container">
+            {
+                alert==true? <div className='alert alert-warning'>2초 후에 사라지는 창</div> : null
+            }
+            
             {count}
-            <button onClick={()=>{setCount(count+1)}}>버튼</button>
+            <button className="btn btn-danger" onClick={()=>{setCount(count+1)}}>좋아요</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes"+(Number(usernum)+1)+".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
+                    <input type='text'></input>
                     <h4 className="pt-5">{상품.title}</h4>
                     <p>{상품.content}</p>
                     <p>{상품.price}</p>
--- shoppingmall/src/compo/Main.js
+++ shoppingmall/src/compo/Main.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import '../App.css';
-
+import { useNavigate } from 'react-router-dom';
 const Main = (props) => {
 
-
+let navigate=useNavigate();
     
     return (
         <>
@@ -14,7 +14,8 @@ const Main = (props) => {
                         (props.shoes).map((a, i) => { //props로 부모 컴포넌트의 shoes state 가져오고 map 함수로 state 데이터 요소갯수만큼 코드 반복
                             return (
                                 <div className='col-md-4'>
-                                    <img src={'https://codingapple1.github.io/shop/shoes' + (i + 1) + '.jpg'} />
+                                    <img src={'https://codingapple1.github.io/shop/shoes' + (i + 1) + '.jpg'}
+                                    onClick={()=>{navigate('/cart/'+ i)}}/> {/*이미지 클릭하면 cart 라우터로 이동*/}
                                     <h4>{props.shoes[i].title}</h4>
                                     <p>{props.shoes[i].content}</p>
                                     <p>{props.shoes[i].price}원</p>
