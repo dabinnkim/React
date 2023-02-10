bf8ee3624ae0ff97cd990ad6b9225373bfdf38c5
kdb <dabin6743@gmail.com>
2023-02-10 15:26:38 +0900

    useEffect

--- shoppingmall/src/compo/Cart.js
+++ shoppingmall/src/compo/Cart.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {useParams, useSearchParams} from 'react-router-dom'
 
 const Cart = (props) => {
+
+    useEffect(()=>{
+        console.log('ㅎㅇ')
+    })
+
+    let[count,setCount] = useState(0)
 
     let {usernum} = useParams(); // 사용자가 입력한 url 파라미터를 가져와줌
 
@@ -11,9 +17,11 @@ const Cart = (props) => {
 
     return (
         <div className="container">
+            {count}
+            <button onClick={()=>{setCount(count+1)}}>버튼</button>
             <div className="row">
                 <div className="col-md-6">
-                    <img src={"https://codingapple1.github.io/shop/shoes"+(Number(id)+1)+".jpg"} width="100%" />
+                    <img src={"https://codingapple1.github.io/shop/shoes"+(Number(usernum)+1)+".jpg"} width="100%" />
                 </div>
                 <div className="col-md-6">
                     <h4 className="pt-5">{상품.title}</h4>
