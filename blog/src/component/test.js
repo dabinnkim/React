a6a0b11f20079cdd52ab4d78200c405a5b88e7a7
kdb <dabin6743@gmail.com>
2023-03-14 17:50:38 +0900

    Docs : 파일삭제

--- blog/src/component/test.js
+++
@@ -1,9 +0,0 @@
-function solution(array) {
-    //배열 오름차순
-    var answer = array.sort((a,b)=>a-b);
-    //배열길이 나누기 2의 정수 값
-    var num = Math.floor((array.length)/2)
-    return answer[num];
-}
-
-console.log(solution([1,2,7,10,11]))
